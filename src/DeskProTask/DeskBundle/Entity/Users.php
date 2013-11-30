bcbcd1824ffbeae2bf0add0c36a029102bfb165d
unknown <antoniodp@gmail.com>
2013-11-30 15:36:06 +0100

    Test Doctrine entity

---
+++ src/DeskProTask/DeskBundle/Entity/Users.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace DeskProTask\DeskBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Users
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="DeskProTask\DeskBundle\Entity\UsersRepository")
+ */
+class Users
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="text")
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="mail", type="text")
+     */
+    private $mail;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="avatar", type="text")
+     */
+    private $avatar;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Users
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set mail
+     *
+     * @param string $mail
+     * @return Users
+     */
+    public function setMail($mail)
+    {
+        $this->mail = $mail;
+    
+        return $this;
+    }
+
+    /**
+     * Get mail
+     *
+     * @return string 
+     */
+    public function getMail()
+    {
+        return $this->mail;
+    }
+
+    /**
+     * Set avatar
+     *
+     * @param string $avatar
+     * @return Users
+     */
+    public function setAvatar($avatar)
+    {
+        $this->avatar = $avatar;
+    
+        return $this;
+    }
+
+    /**
+     * Get avatar
+     *
+     * @return string 
+     */
+    public function getAvatar()
+    {
+        return $this->avatar;
+    }
+}
